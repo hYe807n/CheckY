7bd563078980c752b0503c79671af50666e1c015
hYe807n <rlagkdus55@gmail.com>
2023-06-12 07:03:32 +0900

    final-demo

--- app/src/main/java/com/example/nfcdemo/ui/checkin/CheckinFragment.kt
+++ app/src/main/java/com/example/nfcdemo/ui/checkin/CheckinFragment.kt
@@ -28,10 +28,7 @@ class CheckinFragment : Fragment() {
         _binding = FragmentCheckinBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textCheckin
-        checkinViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+
         return root
     }
 
